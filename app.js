c1becedd35f1d6373c90477f6ef100bcb6505148
grace <grace@macawesome.fritz.box>
2022-08-16 23:30:10 +0200

    feat:a.js

--- app.js
+++ app.js
@@ -77,7 +77,7 @@ app.get('/:shortUrl', async (req, res) => {
 
 app.post('/shortUrls', async (request, response) => {
   if (request.method == 'POST') {
-    var body = '';
+    const body = '';
 
     request.on('data', function (data) {
       body += data;
